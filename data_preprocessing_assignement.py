54cc4f983994a8a0a43470c799ee1f10427599ad
HouariZegai <houarizegai14@gmail.com>
2019-08-12 01:51:49 +0100

    Fix issue in split data (train/test split)

--- data_preprocessing_assignement.py
+++ data_preprocessing_assignement.py
@@ -44,4 +44,4 @@ X = scaler.fit_transform(X)
 
 # Final Step: Train/Test Splitting.
 from sklearn.model_selection import train_test_split
-train_x, train_y, test_x, test_y = train_test_split(X, y, test_size = 0.3, random_state = 19)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 19)
