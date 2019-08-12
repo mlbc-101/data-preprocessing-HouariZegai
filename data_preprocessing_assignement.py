f2419a079efa32c93a2359f8632745b55cf3d9fc
HouariZegai <houarizegai14@gmail.com>
2019-08-12 01:28:55 +0100

    Finish with preprocessing task :D

--- data_preprocessing_assignement.py
+++ data_preprocessing_assignement.py
@@ -19,21 +19,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Step 1: load your dataset using pandas
-data = pd.read_csv('D:\Learn\SelfLearn\ML\Practise\data-preprocessing-HouariZegai\Social_Network_Ads.csv')
-X = data.iloc[:, :4]
-y = data.iloc[:,4]
+data = pd.read_csv('Social_Network_Ads.csv')
+data.head()
+
+X = data.drop(['User ID', 'Purchased'], axis = 1)
+y = data['Purchased']
 
 # Step 2: Handle Missing data if they exist.
-from sklearn.preprocessing import Imputer
-im = Imputer()
-X[:, 1:4] = im.fit_transform(X[:, 1:4])
+# summarize missing data (check if there is a missing data)
+X.isnull().sum()
 
 # Step 3: Encode the categorical variables.
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-lbl_x = LabelEncoder()
-y = lbl_x.fit_transform(y)
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+X['Gender'] = encoder.fit_transform(X['Gender'])
 
+X.head()
 
 # Step 4: Do Feature Scaling if necessary.
+from sklearn.preprocessing import StandardScaler
+# standardise the dataset
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 
 # Final Step: Train/Test Splitting.
+from sklearn.model_selection import train_test_split
+train_x, train_y, test_x, test_y = train_test_split(X, y, test_size = 0.3, random_state = 19)
